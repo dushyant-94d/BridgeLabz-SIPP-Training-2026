d45e0ba7a1116ee4f4866ca1bc6592504a56221b
dushyant-94d <sharmadushyany9456@gmail.com>
2026-07-16 09:22:47 +0530

    Linked List

---
+++ generics/gcr-codebase/scenerio-based/LinkedList/Node.java
@@ -0,0 +1,19 @@
+class Node {
+    int val;
+    Node next;
+
+    Node(int val) {
+        this.val = val;
+    }
+}
+
+void insertAfter(Node current, int trackId) {
+
+    if (current == null)
+        return;
+
+    Node newNode = new Node(trackId);
+
+    newNode.next = current.next;
+    current.next = newNode;
+}
