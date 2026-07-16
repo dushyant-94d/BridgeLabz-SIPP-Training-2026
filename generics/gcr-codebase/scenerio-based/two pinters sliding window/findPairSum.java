856cc4fe993bb1e3da2368bc0ddaa99e8ba0a250
dushyant-94d <sharmadushyany9456@gmail.com>
2026-07-16 09:20:36 +0530

    two pointer

---
+++ generics/gcr-codebase/scenerio-based/two pinters sliding window/findPairSum.java
@@ -0,0 +1,20 @@
+public int[] findPairSum(int[] transactions, int target) {
+
+    int left = 0;
+    int right = transactions.length - 1;
+
+    while (left < right) {
+
+        int sum = transactions[left] + transactions[right];
+
+        if (sum == target)
+            return new int[]{transactions[left], transactions[right]};
+
+        if (sum < target)
+            left++;
+        else
+            right--;
+    }
+
+    return new int[]{-1, -1};
+}
